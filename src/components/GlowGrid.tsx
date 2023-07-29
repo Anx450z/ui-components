0e8c38969c69199f28d66e218b6cb2e125080de9
Ankur <anx450z@gmail.com>
2023-07-29 12:47:42 +0530

    first commit

---
+++ src/components/GlowGrid.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './GlowGrid.css'
+
+const GlowGrid = () => {
+
+  const updateMouseMovement = (e) => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      (card as HTMLElement).style.setProperty("--mouse-x", `${x}px`);
+      (card as HTMLElement).style.setProperty("--mouse-y", `${y}px`);
+    }
+  }
+
+  return(
+ <>
+  <div className='card' onMouseMove={(e)=>updateMouseMovement(e)}>
+    <div className='card-content'>
+      this
+    </div>
+  </div>
+ </>
+  )
+};
+
+export default GlowGrid;
